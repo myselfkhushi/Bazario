8e659cb4e297a5aad296a3c665009e1a2def6321
khushi kumari <khushikumari882484@gmail.com>
2026-09-10 22:26:05 +0530

    Baseurl Change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const api= axios.create({
-    baseURL:"http://localhost:5000/api",
-    withCredentials:true,
-    // headers:{
-    //     "Content-Type":"application/json",
-    // },
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 });
 
 export default api;
